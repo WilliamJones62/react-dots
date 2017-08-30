63fabc1bc9a45a73cca73da06074663c39df318a
williamjones62 <williamjones62@gmail.com>
2017-08-29 20:41:31 -0400

    Show player number when square completed

--- src/App.js
+++ src/App.js
@@ -28,34 +28,44 @@ function Vertical(props) {
 function Square(props) {
   return (
     <button className="square">
+      {props.value}
     </button>
   );
 }
 
 class Board extends React.Component {
   constructor() {
-     super();
-     this.state = {
-       verticals: Array(36).fill(null),
-       horizontals: Array(36).fill(null),
-       squares: Array(36).fill(0),
-       oneIsNext: true,
-     };
-   }
+    super();
+    this.state = {
+      verticals: Array(36).fill(null),
+      horizontals: Array(36).fill(null),
+      counters: Array(36).fill(0),
+      squares: Array(36).fill(null),
+      oneIsNext: true,
+    };
+  }
 
   handleHorizontalClick(i) {
     const horizontals = this.state.horizontals.slice();
+    const counters = this.state.counters.slice();
     const squares = this.state.squares.slice();
     if (horizontals[i] || ((i+1) % 6 === 0)) {
       return;
     }
     horizontals[i] = true;
-    squares[i]++;
+    counters[i]++;
+    if (counters[i] === 4) {
+      squares[i] = this.state.oneIsNext ? "1" : "2";
+    }
     if (i > 5){
-      squares[i-6]++;
+      counters[i-6]++;
+      if (counters[i-6] === 4) {
+        squares[i-6] = this.state.oneIsNext ? "1" : "2";
+      }
     }
     this.setState({
       horizontals: horizontals,
+      counters: counters,
       squares: squares,
       oneIsNext: !this.state.oneIsNext,
     });
@@ -72,20 +82,29 @@ class Board extends React.Component {
 
   handleVerticalClick(i) {
     const verticals = this.state.verticals.slice();
+    const counters = this.state.counters.slice();
     const squares = this.state.squares.slice();
     if (verticals[i]) {
       return;
     }
     verticals[i] = true;
-    squares[i]++;
-    if(i > 0){
-      squares[i-1]++;
+    counters[i]++;
+    if (counters[i] === 4) {
+      squares[i] = this.state.oneIsNext ? "1" : "2";
+    }
+    if (i > 0) {
+      counters[i-1]++;
+      if (counters[i-1] === 4) {
+        squares[i-1] = this.state.oneIsNext ? "1" : "2";
+      }
     };
     this.setState({
       verticals: verticals,
+      counters: counters,
       squares: squares,
       oneIsNext: !this.state.oneIsNext,
     });
+
   }
 
   renderVertical(i) {
@@ -100,6 +119,7 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
+        value={this.state.squares[i]}
       />
     );
   }
